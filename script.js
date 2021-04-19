7fe20ab7306fb9be0695e706704336e9f60074e1
charlineee <charlineee@users.noreply.github.com>
2021-04-19 14:47:41 -0700

    added files to repo

---
+++ script.js
@@ -0,0 +1,114 @@
+
+//format date
+function formatDate() {
+  let now = new Date();
+  return `${
+    now.getMonth() + 1
+  }/${now.getDate()}/${now.getFullYear()}, ${now.getHours()}:${now.getMinutes()}`;
+}
+
+//add date to h2
+function autoDate() {
+  let currentDate = document.querySelector("#date-time");
+  currentDate.innerHTML = formatDate();
+}
+autoDate();
+
+//convert c/f
+function cToF(c) {
+  return (c * 9) / 5 + 32;
+}
+function fToC(f) {
+  return ((f - 32) * 5) / 9;
+}
+
+
+//update temp with conversion
+
+let fahrenheit = document.querySelector("#fah");
+let celsius = document.querySelector("#cel");
+
+celsius.addEventListener("click", toggleC);
+fahrenheit.addEventListener("click", toggleF);
+
+function toggleC() {
+  let myTemp = document.querySelector("#cur-temp");
+  myTemp.innerHTML = "21°";
+}
+
+function toggleF() {
+  let myTemp = document.querySelector("#cur-temp");
+  myTemp.innerHTML = "70°";
+}
+
+
+//update temperature and city name
+function showWeather(response) {
+  let updatedTemp = document.querySelector("#cur-temp");
+  let temperature = Math.round(response.data.main.temp);
+  updatedTemp.innerHTML = `${temperature}°`;
+  let currentCity = document.querySelector("#def-location");
+  let cityName = response.data.name;
+  currentCity.innerHTML = cityName;
+  let humidity = response.data.main.humidity;
+  let data-wind = response.data.wind.speed;
+  let data-status = response.data.weather.0.description;
+  let wind = document.querySelector("#wind");
+  let hum = document.querySelector("#hum");
+  wind.innerHTML = data-wind;
+  hum.innerHTML = humidity;
+
+
+}
+//get current location
+
+let getCurrentLoc = document.querySelector("#current-loc-button");
+getCurrentLoc.addEventListener("click", getPosition);
+
+function getPosition(){
+	navigator.geolocation.getCurrentPosition(showPosition);
+}
+
+
+function showPosition(position) {
+  
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiKey = "cb12dde16b4b5750157657ccf03c3f57";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+
+  axios.get(apiUrl).then(showWeather);
+}
+
+
+
+//search by name
+
+let searchBar = document.querySelector("form");
+searchBar.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let input = document.querySelector("#search-bar");
+  let usersCity = input.value.toLowerCase().trim();
+  let apiKey = "cb12dde16b4b5750157657ccf03c3f57";
+  let units = "metric";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${usersCity}&appid=${apiKey}&units=${units}`;
+  
+  axios.get(apiUrl).then(showWeather);
+}
+
+
+
+//if time < 18:00
+//rainy
+//windy
+//sunny
+//overcast
+//snowy
+//else if time is > 1800
+//stormy
+//clear
+//rain
+//snowy

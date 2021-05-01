1f074a5428a3d0880fad705d65cb99b70bfb3e6a
charlineee <charlineee@users.noreply.github.com>
2021-05-01 15:24:23 -0700

    added background and 7 day forecast js

--- script.js
+++ script.js
@@ -15,7 +15,13 @@ function autoDate() {
 }
 autoDate();
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
 
 //update temp with conversion
 
@@ -44,10 +50,37 @@ function toggleF() {
   
 }
 
-
+function loadVid(status) {
+	let bgVideo = document.querySelector("#myVideo");
+	key = status.toLowerCase();
+	if (key.includes("cloud")) {
+    bgVideo.setAttribute(
+    "src",
+    `media/cloudy.mp4`);
+	} else if (key.includes("rain") || key.includes("mist")) {
+	bgVideo.setAttribute(
+    "src",
+    `media/rainy.mp4`);
+	} else if (key.includes("clear")) {
+	bgVideo.setAttribute(
+    "src",
+    `media/grassy.mp4`);
+	}else if (key.includes("snow")) {
+	bgVideo.setAttribute(
+    "src",
+    `media/snowy.mp4`);
+	}
+	else if (key.includes("wind")) {
+	bgVideo.setAttribute(
+    "src",
+    `media/windy.mp4`);
+	}
+}
 
 //update weather to html
 function showWeather(response) {
+  let lat = response.data.coord.lat;
+  let lon = response.data.coord.lon;
 
   let updatedTemp = document.querySelector("#cur-temp");
   fahTemp = Math.round(response.data.main.temp);
@@ -69,6 +102,8 @@ function showWeather(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(lat, lon);
+  loadVid(statusMain);
 }
 
 
@@ -96,7 +131,6 @@ function showPosition(position) {
 let fahTemp = null;
 
 
-
 //search by name
 
 let searchBar = document.querySelector("form");
@@ -106,14 +140,61 @@ function handleSubmit(event) {
   event.preventDefault();
   let input = document.querySelector("#search-bar");
   let usersCity = input.value.toLowerCase().trim();
+  search(usersCity);
+}
+
+
+//get 7 day forecast
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 7) {
+      forecastHTML =
+        forecastHTML +
+        ` <li>
+				<div class="col-2" id= "7-day-elements">
+				${formatDay(forecastDay.dt)}</strong> <img
+					src="http://openweathermap.org/img/wn/${
+					forecastDay.weather[0].icon
+					}@2x.png"
+					alt=""
+					width="42"
+					/>
+				<span class="weather-forecast-temperature-max"> ${Math.round(
+				forecastDay.temp.max
+				)}° </span>
+				<span class="weather-forecast-temperature-min"> ${Math.round(
+				forecastDay.temp.min
+				)}° </span>
+        		
+      			</div>
+	  			</li>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+//retrieve 7 day forecast data
+function getForecast(lat, lon){
   let apiKey = "cb12dde16b4b5750157657ccf03c3f57";
   let units = "imperial";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${usersCity}&appid=${apiKey}&units=${units}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+function search(city){
+  let apiKey = "cb12dde16b4b5750157657ccf03c3f57";
+  let units = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
   
   axios.get(apiUrl).then(showWeather);
 }
 
-
-
-
-
+//default city
+search("San Francisco");
